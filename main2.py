afe8b4be96826483bfb6aa242de0b9ffe897a6cd
Rishabh Anand <mail.rishabh.anand@gmail.com>
2022-09-13 11:21:19 +0800

    Updates to code

--- main2.py
+++ main2.py
@@ -135,8 +135,7 @@ if __name__ == '__main__':
         cfg.run_id = run_id
         seed_everything(cfg.seed)
         auto_select_device()
-        if cfg.train.finetune:
-            cfg = load_pretrained_model_cfg(cfg)
+
         logging.info(f"[*] Run ID {run_id}: seed={cfg.seed}, "
                      f"split_index={cfg.dataset.split_index}")
         logging.info(f"    Starting now: {datetime.datetime.now()}")
